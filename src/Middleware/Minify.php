176b3e6884f3f3f5d4e6fdef30010da2ab42ea2c
Sergey Podgornyy <Serhii.Podhornyi@check24.de>
2018-08-08 17:46:47 +0200

    Cleaning code and add test coverage

--- src/Middleware/Minify.php
+++ src/Middleware/Minify.php
@@ -2,10 +2,31 @@
 
 namespace Ken\BladeMinify\Middleware;
 
-use Closure, Config;
+use Closure;
+use Config;
 
 class Minify
 {
+    const DEFAULT_REPLACEMENT = [
+        '/<!--[^\[](.*?)[^\]]-->/s' => '',
+        "/<\?php/"                  => '<?php ',
+        "/\n([\S])/"                => '$1',
+        "/\r/"                      => '',
+        "/\n/"                      => '',
+        "/\t/"                      => '',
+        '/ +/'                      => ' ',
+        '/> +</'                    => '><',
+    ];
+
+    const SPECIFIC_REPLACEMENT = [
+        '/<!--[^\[](.*?)[^\]]-->/s' => '',
+        "/<\?php/"                  => '<?php ',
+        "/\r/"                      => '',
+        "/>\n</"                    => '><',
+        "/>\s+\n</"                 => '><',
+        "/>\n\s+</"                 => '><',
+    ];
+
     /**
      * Handle an incoming request.
      *
@@ -13,7 +34,6 @@ class Minify
      * @param  \Closure $next
      * @return mixed
      */
-
     public function handle($request, Closure $next)
     {
         $response = $next($request);
@@ -25,33 +45,28 @@ class Minify
     public function html($response)
     {
         $buffer = $response->getContent();
-
-        $replace = [
-            '/<!--[^\[](.*?)[^\]]-->/s' => '',
-            "/<\?php/" => '<?php ',
-            "/\n([\S])/" => '$1',
-            "/\r/" => '',
-            "/\n/" => '',
-            "/\t/" => '',
-            "/ +/" => ' ',
-            "/> +</" => '><',
-        ];
-
-        if(strpos($buffer,'<pre') !== false || strpos($buffer,'&lt;pre') !== false || strpos($buffer,'//') !== false) {
-            $replace = [
-                '/<!--[^\[](.*?)[^\]]-->/s' => '',
-                "/<\?php/" => '<?php ',
-                "/\r/" => '',
-                "/>\n</" => '><',
-                "/>\s+\n</" => '><',
-                "/>\n\s+</" => '><',
-            ];
-        }
+        $replace = $this->shouldReplaceSpecial($buffer)
+            ? self::SPECIFIC_REPLACEMENT
+            : self::DEFAULT_REPLACEMENT;
 
         $buffer = preg_replace(array_keys($replace), array_values($replace), $buffer);
 
         $response->setContent($buffer);
 
         return $response;
+    }
+
+    /**
+     * Check should special RegEx rules be applied
+     *
+     * @param string $buffer
+     *
+     * @return bool
+     */
+    private function shouldReplaceSpecial($buffer)
+    {
+        return strpos($buffer, '<pre') !== false
+            || strpos($buffer, '&lt;pre') !== false
+            || strpos($buffer, '//') !== false;
     }
 }
